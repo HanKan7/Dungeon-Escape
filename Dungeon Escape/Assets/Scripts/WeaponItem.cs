5cff7f34664fce2de4b060d927fa02207206f607
Hanu Kancharla <kancharla.hanu@gmail.com>
2021-11-28 19:58:03 -0800

    Weapon Idle Animation

--- Dungeon Escape/Assets/Scripts/WeaponItem.cs
+++ Dungeon Escape/Assets/Scripts/WeaponItem.cs
@@ -8,6 +8,11 @@ public class WeaponItem : Item
     public GameObject modelPrefab;
     public bool isUnarmed;
 
+    [Header("Idle Animation")]
+    public string right_Hand_Idle;
+    public string left_Hand_Idle;
+
+
     [Header("One Handed Attack Animation")]
     public string OH_Light_Attack_01;
     public string OH_Heavy_Attack_01;
