a5be85c0ef5bd98a7d52288bfa762c8cad27d64b
Hanu Kancharla <kancharla.hanu@gmail.com>
2021-12-01 18:53:04 -0800

    Inventory Panel

--- Dungeon Escape/Assets/UIManager.cs
+++ Dungeon Escape/Assets/UIManager.cs
@@ -5,7 +5,37 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    public PlayerInventory playerInventory;
+
     public GameObject selectWindow;
+
+    [Header("Weapons Inventory")]
+    public GameObject weaponInventorySlotPrefab;
+    WeaponInventorySlot[] weaponsInvSlots;
+    public Transform weaponInvSlotsParent;
+
+    public void UpdateUI()
+    {
+        #region weaponsInvSlots
+        for(int i = 0; i < weaponsInvSlots.Length; i++)
+        {
+            if(i < playerInventory.weaponsInventory.Count)
+            {
+                if(weaponsInvSlots.Length < playerInventory.weaponsInventory.Count)
+                {
+                    Instantiate(weaponInventorySlotPrefab, weaponInvSlotsParent);
+                    weaponsInvSlots = weaponInvSlotsParent.GetComponentsInChildren<WeaponInventorySlot>();
+                }
+                weaponsInvSlots[i].AddItem(playerInventory.weaponsInventory[i]);
+                
+            }
+            else
+            {
+                weaponsInvSlots[i].ClearInventorySlot();
+            }
+        }
+        #endregion
+    }
 
     public void OpenSelectWindow()
     {
